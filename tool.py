405184ac747d9d20ccb3e5fdb34e8d3be483c6c4
lovetonight <hoan.khoc@gmail.com>
2023-06-13 00:30:44 +0700

    get massage

--- tool.py
+++ tool.py
@@ -1,5 +1,9 @@
+import datetime
 from telethon import TelegramClient, events, sync
 import time
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.channels import GetMessagesRequest
+
 
 from telethon.errors.rpcerrorlist import PeerIdInvalidError
 # These example values won't work. You must get your own api_id and
@@ -19,6 +23,40 @@ client1.start()
 clients = {client1}  # Các account
 
 
+'''
+Lấy toàn bộ tin nhắn từ start_id tới end_id
+username: username của người dùng hoặc group
+
+'''
+
+#TODO: Get tin nhắn từ channel
+def getMessage(client, username, start_id, end_id):
+    try:
+
+        entity = client.get_entity(username)
+        chat_id = entity.id
+        messages = client.get_messages(entity, limit=1000) #TODO: Chỉnh limit sao cho hợp lý
+        message_ids = [msg.id for msg in messages]
+        for message in messages:
+            if message.id >= start_id and message.id <= end_id:
+                print(message.message)
+
+    except ValueError:
+        print("Chat ID không hợp lệ")
+    except Exception as e:
+        print(f"Lỗi khi lấy nội dung cuộc trò chuyện: {str(e)}")
+
+    return
+
+
+'''
+Gửi/ xóa tin nhắn tới user hoặc group
+
+Gửi tin nhắn có thể reply tin nhắn khác, bằng cách thay đổi 'reply_message_id' thành id của tin nhắn muốn reply
+
+Delete tin nhắn trong trò chuyện với user có thể xóa tin nhắn của đối phương
+'''
+
 for client in clients:
     try:
         entity = client.get_entity(username)
@@ -28,9 +66,14 @@ for client in clients:
         reply_message = 'Hello'  # Kịch bản nhắn tin
         message_ids = [msg.id for msg in messages]
         # client.delete_messages(entity, message_ids)
-        client.send_message(entity, reply_message, reply_to=3)
+        # reply_message_id = 3
+        # client.send_message(entity, reply_message, reply_to=reply_message_id)
         # time.sleep(2)
+
     except ValueError:
         print("Chat ID không hợp lệ")
     except Exception as e:
         print(f"Lỗi khi lấy nội dung cuộc trò chuyện: {str(e)}")
+
+
+getMessage(client1, 'CHINCRYPTO_CHAT', 17622, 17647)
